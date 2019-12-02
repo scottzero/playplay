aa37c1d6d234024dd264ba334bba3aa9f06c1d89
Scott Payton <spayton.c@gmail.com>
2019-12-02 14:34:15 -0700

    Added heroku postgres URL to knex file

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: '',
+    connection: 'postgres://qvevezsuqcndls:0ec04bd48ce3a2c66c799130cd2d69f164666588cd05e1932eaf05ec5d593a18@ec2-174-129-255-76.compute-1.amazonaws.com:5432/dc1f5hm1gd3viu',
     migrations: {
       directory: './db/migrations'
     },
