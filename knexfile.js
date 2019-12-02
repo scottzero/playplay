29529fe56c89a02d83b3dbd6f437cf91926a5e7c
Scott Payton <spayton.c@gmail.com>
2019-12-02 13:55:24 -0700

    Co-authored-by: Nancy Lee <24424825+nancylee713@users.noreply.github.com>

---
+++ knexfile.js
@@ -0,0 +1,32 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/publications_dev',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/publications_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: '',
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
+};
