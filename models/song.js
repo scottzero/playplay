3385e8e47577efdab1fade973ce855ba33f0993f
Scott Payton <spayton.c@gmail.com>
2019-12-03 13:11:16 -0700

    Create POJO to handle song data filtering
    Co-authored-by: Nancy Lee <24424825+nancylee713@users.noreply.github.com>

--- models/song.js
+++ models/song.js
@@ -4,9 +4,7 @@ class Song{
     this.title = songData.track.track_name
     this.artistName = songData.track.artist_name
     this.rating = songData.track.track_rating
-    this.genre = songData.track.primary_genres.music_genre
-    // console.log(this.genre)
-    // console.log(this.genre = songData.track.primary_genres)
+    this.genre = songData.track.primary_genres.music_genre_list[0].music_genre.music_genre_name
   }
 }
 module.exports = Song
