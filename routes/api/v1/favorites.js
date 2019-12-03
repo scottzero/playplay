e1e3e21927c72b85c72f225771a5cb7e51b719c4
nancylee713 <srlee713@gmail.com>
2019-12-03 11:26:05 -0700

    Change api methods in request endpoint
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -12,18 +12,15 @@ const getKey = (apikey) => {
 }
 async function getSong(track, artist){
   const key = await getKey('apikey');
-  // try {
-    let response = await fetch(`https://api.musixmatch.com/ws/1.1/track.search?q_track=${track}&q_artist=${artist}&quorum_factor=0.9&apikey=${key}`)
+  try {
+    let response = await fetch(`https://api.musixmatch.com/ws/1.1/matcher.track.get?q_track=${track}&q_artist=${artist}&apikey=${key}`)
     let song = await response.json();
-    let re_artist = await new RegExp(`^${artist}$`)
-    // let re_track = await new RegExp(`^${track}$`)
-    // let regEx = await /^art$/;
-    let data = await song.message.body.track_list.find(obj => ( re_artist.test(obj.track.artist_name) && obj.track.track_name.includes(track)));
-    console.log(data);
-  // } catch(err){
-    // return err;
-  // }
+    let data = await song.message.body.track;
+    return data;
+  } catch(err){
+    return err;
+  }
 }
 
 
-getSong("Creep", "Radiohead")
+getSong("Creep", "Radiohead").then(res => console.log(res));
