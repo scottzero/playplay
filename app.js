4c5f54a6808e339584671b636cb36cb8e4115590
Scott Payton <spayton.c@gmail.com>
2019-12-02 17:32:01 -0700

    Create favorites async method to fetch songs
    Co-authored-by: Nancy Lee <24424825+nancylee713@users.noreply.github.com>

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
-
 var indexRouter = require('./routes/index');
-var papersRouter = require('./routes/api/v1/papers');
+var favoritesRouter = require('./routes/api/v1/favorites');
 
 var app = express();
 
@@ -18,6 +16,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/v1/papers', papersRouter);
+app.use('/api/v1/favorites', favoritesRouter);
 
 module.exports = app;
