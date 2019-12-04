e78de9b55b23321c0b7eceee3df05275fae083d5
nancylee713 <srlee713@gmail.com>
2019-12-04 11:13:52 -0700

    Reconfigure knexfile and environment variables

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,4 @@
+const env = require('dotenv').config();
 // Update with your config settings.
 
 module.exports = {
@@ -23,15 +24,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'postgres://qvevezsuqcndls:0ec04bd48ce3a2c66c799130cd2d69f164666588cd05e1932eaf05ec5d593a18@ec2-174-129-255-76.compute-1.amazonaws.com:5432/dc1f5hm1gd3viu',
-    migrations: {
-      directory: './db/migrations'
-    },
-    useNullAsDefault: true
-  },
-  staging: {
-    client: 'pg',
-    connection: 'postgres://xbcnwfdfehzhxe:9bf75777e8665b7dc7b3ebf776727b8ef18fdf3ebb90bf0d9ad02a46ffb40af0@ec2-174-129-255-46.compute-1.amazonaws.com:5432/ddr2mem80li8i8',
+    connection: `${process.env.DATABASE_URL}`,
     migrations: {
       directory: './db/migrations'
     },
