d629613fe4996077284497902dda714f38f45a56
nancylee713 <srlee713@gmail.com>
2019-12-05 13:20:59 -0700

    Add GET endpoint and playlistPojo
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>

---
+++ models/playlist.js
@@ -0,0 +1,10 @@
+//playlist pojo
+
+class Playlist{
+  constructor(playlistData){
+    this.title = playlistData.title;
+    this.createdAt = playlistData.created_at;
+    this.updatedAt = playlistData.updated_at;
+  }
+}
+module.exports = Playlist;
