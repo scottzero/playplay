76d15579f85f4dcf87eb494aad4b9b004ebca24e
nancylee713 <srlee713@gmail.com>
2019-12-05 15:56:00 -0700

    Add put endpoint test for playlists, including happy and sad paths
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>

--- models/playlist.js
+++ models/playlist.js
@@ -2,6 +2,7 @@
 
 class Playlist{
   constructor(playlistData){
+    this.id = playlistData.id;
     this.title = playlistData.title;
     this.createdAt = playlistData.created_at;
     this.updatedAt = playlistData.updated_at;
