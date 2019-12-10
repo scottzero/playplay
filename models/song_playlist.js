8abe6096bf596b68b0c519aadc681ba5475ff8a8
nancylee713 <srlee713@gmail.com>
2019-12-10 15:41:24 -0700

    Add static summary method to songPlaylist model for data formatting
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>

--- models/song_playlist.js
+++ models/song_playlist.js
@@ -1,13 +1,50 @@
 //playlist pojo
 const database = require('../config')
+const playlist = require('../models/playlist')
 
 class songPlaylist{
+  // constructor(playlistData){
+  //   this.id = playlistData.id;
+  //   this.title = playlistData.title;
+  //   this.songCount = playlistData.songCount;
+  //   this.songAvgRating = playlistData.songAvgRating;
+  //   this.favorites = playlistData.favorites;
+  //   this.createdAt = playlistData.created_at;
+  //   this.updatedAt = playlistData.updated_at;
+  // }
+
   static add(favoriteID, playlistID) {
     return database('favorites_playlists').insert({favorite_id: favoriteID, playlist_id: playlistID}, "id")
   }
 
   static find(favoriteID, playlistID) {
     return database('favorites_playlists').where({favorite_id: favoriteID, playlist_id: playlistID})
+  }
+
+  static getPlaylistData(playlistID) {
+    return database('favorites_playlists').where('playlist_id', playlistID)
+      .join('favorites', {'favorites_playlists.favorite_id': 'favorites.id'})
+      // .avg('rating')
+  }
+
+  static getFavorites(playlistID) {
+    return database('favorites_playlists')
+      .where('playlist_id', playlistID)
+      .join('favorites', {'favorites_playlists.favorite_id': 'favorites.id'})
+      .select('favorite_id as id', 'title', 'artistName', 'genre', 'rating')
+  }
+
+
+  static async summary(playlistID) {
+    return {
+      "id": playlistID,
+      "title": await playlist.findPlaylist(playlistID).select('title').then(res => res[0].title),
+      "songCount": await songPlaylist.getPlaylistData(playlistID).then(res => res.length),
+      "songAvgRating": await songPlaylist.getPlaylistData(playlistID).avg('rating').then(res => res[0].avg),
+      "favorites": await songPlaylist.getFavorites(playlistID),
+      "createdAt": await playlist.findPlaylist(playlistID).select('created_at').then(res => res[0].created_at),
+      "updatedAt": await playlist.findPlaylist(playlistID).select('updated_at').then(res => res[0].updated_at)
+    }
   }
 }
 
