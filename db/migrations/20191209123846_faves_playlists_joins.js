f4b9a5ff8be2d475f73ffc1623125d1c3b0c51aa
nancylee713 <srlee713@gmail.com>
2019-12-09 21:58:24 -0700

    Add on delete, on update cascade to favorites_playlists table
    Co-authored-by: Scott Payton <33855435+scottzero@users.noreply.github.com>

--- db/migrations/20191209123846_faves_playlists_joins.js
+++ db/migrations/20191209123846_faves_playlists_joins.js
@@ -2,8 +2,12 @@ exports.up = function(knex) {
   return Promise.all([
     knex.schema.createTable('favorites_playlists', function(table){
       table.increments('id').primary();
-      table.integer('favorite_id').references('favorites.id');
-      table.integer('playlist_id').references('playlists.id');
+      table.integer('favorite_id').references('favorites.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      table.integer('playlist_id').references('playlists.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
     })
   ]);
 };
